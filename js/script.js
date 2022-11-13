01d973c6ab3eb2343152bb88f2303c45880699ad
bangsyachrul <syachruletrama12@gmail.com>
2022-11-13 08:44:30 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.sidenav').ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  $('.tooltipped').ready(function(){
+    $('.tooltipped').tooltip();
+  });
+
+  $('.fixed-action-btn').ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
+
+  $('.scrollspy').ready(function(){
+    $('.scrollspy').scrollSpy({
+      scrollOffset: 259
+    });
+  });
